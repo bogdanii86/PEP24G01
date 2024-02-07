a55266a19533abc5d604b6391844073d67b6b5eb
bogdanii86 <ionescu.bogd@gmail.com>
2024-02-07 21:02:04 +0200

    test de commit and push pe github - update

---
+++ modul3/parola.py
@@ -0,0 +1,31 @@
+# cu while
+# parola_pc = '7767'
+# parola = input('Introduceti parola: ')
+# while parola != parola_pc:
+#     parola = input('Parola gresita. Mai incercati.\nIntroduceti parola:')
+# else:
+#     print('Access permis.')
+
+##########################################################
+# cu while - varianta prof
+
+# parola_pc = '7767'
+# while True:
+#     parola = (input('Introduceti parola: '))
+#     if parola_pc == parola:
+#         print("Acces permis.")
+#         break
+#     else:
+#         print('Parola gresita.')
+
+# cu for
+#
+# parola_pc = '7767'
+#
+# for incercare in range(3):
+#     parola = (input('Introduceti parola: '))
+#     if parola_pc == parola:
+#         print("Acces permis.")
+#         break
+#     else:
+#         print('Parola gresita.')
